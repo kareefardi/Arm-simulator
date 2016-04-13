47fb6e59b89c3f8d50d9dab25830b40833f9c57b
kareefardi <karimmhany@gmail.com>
2016-04-13 23:22:44 +0200

    assembler

---
+++ untitled.js
@@ -0,0 +1,167 @@
+function assemble(instr)
+{
+	var code = instr.substr(0, 3);
+	code = code.toUpperCase();
+	console.log('assmeble ' + code);
+	switch (code) {
+	case 'ADC':
+		asmADC(instr); 
+		//add with carry
+		break;
+	case 'ADD':
+		asmADD(instr); 
+		//add variants
+		break;
+	case 'AND':
+		asmAND(instr); 
+		// AND
+		break;
+	case 'ASR':
+		asmASR(instr); 
+		//arithm shift right
+		break;
+	case 'CMN':
+		asmCMN(instr); 
+		//compare negative
+		break;
+	case 'CMP':
+		asmCMP(instr); 
+		//compare
+		break;
+	case 'EOR':
+		asmEOR(instr); 
+		//EOR
+		break;
+	case 'LDM':
+		asmLDMIA(instr); 
+		//loadmultiple
+		break;
+	case 'LDR':
+		asmLDRBnH(instr); 
+		//load byte and load halfword
+		break;
+	case 'LSL':
+		asmLSL(instr); 	  
+		//logical shift left
+		break;
+	case 'LDS':
+		asmLDSBnH(instr); 
+		//load sign ext. byte and halfword
+		break;
+	case 'LSR':
+		asmLSR(instr); 	  
+		//logical shift right
+		break;
+	case 'MOV':
+		asmMOV(instr);	  
+		//move register
+		break;
+	case 'MUL':
+		asmMUL(instr);    
+		//multiply
+		break;
+	case 'MVN':
+		asmMVN(instr);    
+		//move negative register
+		break;
+	case 'NEG':
+		asmNEG(instr);    
+		//negate
+		break;
+	case 'ORR':
+		asmORR(instr);    
+		//OR
+		break;
+	case 'POP':
+		asmPOP(instr);    
+		//pop registers
+		break;
+	case 'PUS':
+		asmPUSH(instr);   
+		//push registers
+		break;
+	case 'ROR':
+		asmROR(instr);    
+		//rotate right
+		break;
+	case 'SBC':
+		asmSBC(instr);    
+		//subtract with carry
+		break;
+	case 'STM':
+		asmSTMIA(instr);  
+		//stire multiple
+		break;
+	case 'STR':
+		asmSTRWnBnH(instr); 
+		//store word or half or byte
+		break;
+	case 'SWI':
+		asmSWI(instr);    
+		//software interrupt    
+		break;
+	case 'SUB':
+		asmSUB(instr);    
+		//subtract 
+		break;
+	case 'TST':
+		asmTST(instr);    
+		//test bits
+		break;
+	default:
+		handleB(instr);    
+		//handle instructions starting with B
+		break;
+	}
+}
+
+function handleB(instr)
+{
+	var code = substr(0, 2);
+	code = code.toUpperCase();
+	switch (code) {
+	case 'BI':
+		asmBIC(instr); 
+		//bit clear
+		break;
+	case 'BL':
+		asmBL(instr); 
+		//branch and link
+		break;
+	case 'BX':
+		asmBX(instr); 
+		//branch and exchange
+		break;
+	case 'B ':
+		asmuncB(instr); 
+		//unconditional branch
+		break;
+	default:
+		asmcnB(instr); 
+		//conditional branch
+		break;
+	}
+}
+
+function asmADC(instr)
+{
+	var out = '010000101';
+	//var res = str.match(/ain/g);
+	var val = instr.match([0-9]+/g);
+	var rd = Number(val[0]).ToString(2);
+	var rm = Number(val[1]).ToString(2);
+}
+
+function foo(sign, len, str)
+{	
+	var res = '';
+	if (sign) {
+		res = str;	
+		for(var i = 0; i < len - str.length - 1; i++)
+			res.push('0');
+	}
+	else {
+		res = str.substring(str.length - len - 1, str.length - 1);
+	}
+	return res;
+}
